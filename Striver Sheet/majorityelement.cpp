2c04b6dd336d85670243fe6a20e604e56fef353b
Aarchishya <59142633+Aarchishya@users.noreply.github.com>
2022-07-02 21:07:07 +0530

    Update majorityelement.cpp

--- Striver Sheet/majorityelement.cpp
+++ Striver Sheet/majorityelement.cpp
@@ -1,1 +1,40 @@
-
+#include <bits/stdc++.h> 
+typedef long long ll;
+int findMajorityElement(int arr[], int n) {
+    ll count=1,flag=0,maxcount=1,fl,ans;
+    sort(arr,arr+n);
+    fl=floor(n/2);
+    if(n==1)
+    {
+        return arr[0];
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]==arr[i+1])
+        {
+            count++;
+            if(count>maxcount)
+            {
+            maxcount=count;
+                ans=arr[i];
+            }
+//             
+        }
+        else
+        {
+            count=1;
+        }
+    }
+        
+    if(maxcount<=fl)
+    {
+        return -1;
+    }
+   
+    if(maxcount>fl)
+    {
+        return ans;
+    }
+    
+	// Write your code here.
+}
