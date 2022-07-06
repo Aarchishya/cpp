58c94e1021d1180f17ccb7e2c1a898df41b462bc
Aarchishya <59142633+Aarchishya@users.noreply.github.com>
2022-07-06 15:46:58 +0530

    Update UniquePaths.cpp

--- Striver Sheet/UniquePaths.cpp
+++ Striver Sheet/UniquePaths.cpp
@@ -43,3 +43,23 @@ int uniquePaths(int m, int n) {
     return ans;
 	// Write your code here.
 }
+
+
+
+#3
+
+
+
+#include <bits/stdc++.h> 
+
+int uniquePaths(int m, int n) {
+    int N=n+m-2;
+    int r=m-1;
+    double res=1;
+    for(int i=1;i<=r;i++)
+    {
+        res=res*(N-r+i)/i;
+    }
+    return (int)res;
+	// Write your code here.
+}
