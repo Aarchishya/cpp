2cb770dbb1bac8a7c6d478d07fee8ecfc587f2b1
Aarchishya <59142633+Aarchishya@users.noreply.github.com>
2022-07-06 12:46:01 +0530

    Update UniquePaths.cpp

--- Striver Sheet/UniquePaths.cpp
+++ Striver Sheet/UniquePaths.cpp
@@ -21,3 +21,25 @@ int uniquePaths(int m, int n) {
     return ans;
 	// Write your code here.
 }
+
+
+
+#2
+
+
+#include <bits/stdc++.h> 
+int countPath(int row,int col,int m,int n,vector<vector<int>>&dp)
+{
+//     int row=0,col=0;
+    if(row>=m||col>=n) return 0;
+    if(row==m-1&&col==n-1) return 1;
+    if(dp[row][col]!=-1) return dp[row][col];
+    else return dp[row][col]=countPath(row+1,col,m,n,dp)+countPath(row,col+1,m,n,dp);
+}
+int uniquePaths(int m, int n) {
+    int row=0,col=0,ans;
+    vector<vector<int>>dp(m,vector<int>(n,-1));
+    ans=countPath(row,col,m,n,dp);
+    return ans;
+	// Write your code here.
+}
