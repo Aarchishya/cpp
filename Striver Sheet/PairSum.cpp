376476e5cc1f4ca5467575df4ff0707a5eac7fda
Aarchishya <59142633+Aarchishya@users.noreply.github.com>
2022-07-12 13:55:09 +0530

    Update PairSum.cpp

--- Striver Sheet/PairSum.cpp
+++ Striver Sheet/PairSum.cpp
@@ -22,3 +22,59 @@ vector<vector<int>> pairSum(vector<int> &arr, int s){
     return ans;
    // Write your code here.
 }
+
+
+#2
+
+
+/*
+
+    Time Complexity: O(N^2)
+    Space COmplexity: O(N)
+
+    Where 'N' is the size of the array.
+    
+*/
+
+#include <map>
+
+vector<vector<int>> pairSum(vector<int> &arr, int s){
+    int n = arr.size();
+
+    // Map to store frequency of elements.
+    map<int, int> hash;
+
+    // This will store the result.
+    vector<vector<int>> ans;
+
+    for (int ele : arr)
+    {
+        int count = hash[s - ele];
+
+        vector<int> pair(2);
+        pair[0] = ele;
+        pair[1] = s - ele;
+
+        while (count--)
+        {
+            ans.push_back(pair);
+        }
+
+        hash[ele] += 1;
+    }
+
+    // This will store the final result.
+    vector<vector<int>> res(ans.size(), vector<int>(2, 0));
+
+    // Sorting the 'ans' arrays element.
+    for (int i = 0; i < ans.size(); i++)
+    {
+        int a = ans[i][0], b = ans[i][1];
+        res[i][0] = min(a, b);
+        res[i][1] = max(a, b);
+    }
+
+    // Finally sorting each pair in 'res'.
+    sort(res.begin(), res.end());
+    return res;
+}
