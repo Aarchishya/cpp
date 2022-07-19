5de36f004d235f4cbb459c2f16268eeb570a7d12
Aarchishya <59142633+Aarchishya@users.noreply.github.com>
2022-07-19 12:09:52 +0530

    Create LongestSubstringwithoutrepeatingchar.cpp

---
+++ Striver Sheet/LongestSubstringwithoutrepeatingchar.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h> 
+int uniqueSubstrings(string input)
+{
+    vector<int>m(256,-1);
+    int left=0,right=0;
+    int n=input.size();
+    int len=0;
+    while(right<n)
+    {
+        if(m[input[right]]!=-1)
+        {
+            left=max(m[input[right]]+1,left);
+        }
+        m[input[right]]=right;
+            len=max(len,right-left+1);
+        right++;
+    }
+    return len; 
+    //Write your code here
+}
