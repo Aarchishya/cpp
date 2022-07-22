294a53428a4ae9733552acf15e78d8b8f60b5b1c
Aarchishya <59142633+Aarchishya@users.noreply.github.com>
2022-07-22 12:36:01 +0530

    Create MergeSortedLinkedList.cpp

---
+++ Striver Sheet/LinkedList/MergeSortedLinkedList.cpp
@@ -0,0 +1,72 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        #include <bits/stdc++.h> 
+/************************************************************
+
+    Following is the linked list node structure.
+    
+    template <typename T>
+    class Node {
+        public:
+        T data;
+        Node* next;
+
+        Node(T data) {
+            next = NULL;
+            this->data = data;
+        }
+
+        ~Node() {
+            if (next != NULL) {
+                delete next;
+            }
+        }
+    };
+
+************************************************************/
+
+
+    if(list1==NULL)
+    {
+        return list2;
+    }
+    if(list2==NULL)
+    {
+        return list1;
+    }
+    if(list1->val>list2->val)
+    {
+        std::swap(list1,list2);
+    }
+    ListNode* res=list1;
+    
+    while(list1!=NULL&&list2!=NULL)
+    {
+       ListNode* temp=NULL;
+        while(list1!=NULL&&list1->val<list2->val)
+        {
+            temp=list1;
+            list1=list1->next;
+        }
+        temp=list2;
+        std::swap(list1,list2);
+    }
+    return res;
+    
+    // Write your code here.
+
+
+        
+    }
+};
