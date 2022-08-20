5d10716b8a18eb444030d77f26fe6ef1bbb2b879
Aarchishya <59142633+Aarchishya@users.noreply.github.com>
2022-08-20 16:27:56 +0530

    Update FractionalKnapsack.cpp

--- Striver Sheet/Greedy/FractionalKnapsack.cpp
+++ Striver Sheet/Greedy/FractionalKnapsack.cpp
@@ -1,1 +1,29 @@
-
+#include<bits/stdc++.h>
+double maximumValue (vector<pair<int, int>>& items, int n, int w)
+{
+    vector<pair<double,int>>vp;
+    for(int i=0;i<n;i++)
+    {
+        double weight=items[i].first;
+        double value=items[i].second;
+        vp.push_back({value/weight,i});
+    }
+    sort(vp.begin(),vp.end());
+    double val=0;
+    for(int i=n-1;i>=0;i--)
+    {
+        int index=vp[i].second;
+        if(items[index].first<w)
+        {
+            w=w-items[index].first;
+            val=val+items[index].second;
+        }
+         else{
+                 val=val+(vp[i].first*w);
+                 break;
+             }
+    }
+    return val;
+    // Write your code here.
+    // ITEMS contains {weight, value} pairs.
+}
