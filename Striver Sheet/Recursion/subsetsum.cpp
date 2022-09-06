48b8b8d4791aa5cbafaa4904a07ffe24d8dedeec
Aarchishya <59142633+Aarchishya@users.noreply.github.com>
2022-09-06 09:55:54 +0530

    Update subsetsum.cpp

--- Striver Sheet/Recursion/subsetsum.cpp
+++ Striver Sheet/Recursion/subsetsum.cpp
@@ -1,1 +1,22 @@
-
+void help(int ind,int sum,vector<int>&ans,vector<int>&num)
+{
+    int n=num.size();
+    if(ind==n)
+    {
+        ans.push_back(sum);
+        return;
+    }
+//     sum=sum=num[ind];
+    help(ind+1,sum+num[ind],ans,num);
+    help(ind+1,sum,ans,num);
+    
+}
+vector<int> subsetSum(vector<int> &num)
+{
+    vector<int>ans;
+    int sum=0;
+    help(0,sum,ans,num);
+    sort(ans.begin(),ans.end());
+    return ans;
+    // Write your code here.
+}
